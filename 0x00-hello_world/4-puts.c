df29584f31efd114b0b73c6cdae9c6d082b2cfce
mefahmy <m@ifhmy.com>
2023-06-16 16:40:10 -0700

    go go

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
-int main (void) {
 
-	puts ("\"Programming is like building a multilingual puzzle") ;
-	return (0) ;
+/**
+ *  that is main -entry 
+ *
+ *  Description : A C program with put
+ *  return: always 0 (success )
+ *
+ */
+
+int main (void)
+{
+	puts ("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
