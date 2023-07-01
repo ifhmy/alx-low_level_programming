7f621be8d17b2dde537abc2c0c7a0b9f209c7f35
Mohamed Fahmy <91461877+ifhmy@users.noreply.github.com>
2023-07-01 12:38:01 +0300

    main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -15,20 +15,3 @@ void print_triangle(int size);
 void print_number(int n);
 
 #endif
-
-_putchar.c
-
-#include "main.h"
-#include <unistd.h>
-    /**
-     * _putchar - writes the character c to stdout
-     * @c: The character to print
-     *
-     * Return: On success 1.
-     * On error, -1 is returned, and errno is set appropriately.
-     */
-    int
-    _putchar(char c)
-{
-    return (write(1, &c, 1));
-}
