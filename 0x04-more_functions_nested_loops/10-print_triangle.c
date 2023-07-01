05aa757a6c221e57a535e4812e4f335bf3bf48ff
Mohamed Fahmy <91461877+ifhmy@users.noreply.github.com>
2023-07-01 12:40:22 +0300

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -5,46 +5,27 @@
  */
 void print_triangle(int size)
 {
-    if (size <= 0)
-    {
-        _putchar('\n');
-    }
-    else
-    {
-        int i, j;
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
 
-        for (i = 1; i <= size; i++)
-        {
-            for (j = i; j < size; j++)
-            {
-                _putchar(' ');
-            }
+		for (i = 1; i <= size; i++)
+		{
+			for (j = i; j < size; j++)
+			{
+				_putchar(' ');
+			}
 
-            for (j = 1; j <= i; j++)
-            {
-                _putchar('#');
-            }
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
 
-            _putchar('\n');
-        }
-    }
-}
-
-10 - main.c
-
-#include "main.h"
-
-     /**
-      * main - check the code
-      *
-      * Return: Always 0.
-      */
-     int
-     main(void)
-{
-    print_triangle(2);
-    print_triangle(10);
-    print_triangle(1);
-    print_triangle(0);
-    return (0);
+			_putchar('\n');
+		}
+	}
 }
